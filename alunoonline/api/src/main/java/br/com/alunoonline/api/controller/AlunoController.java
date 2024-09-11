5d755ff94df2762f0b2ebf0002e388053fd205ee
tomazpantadasilvaneto <tomazpanta@hotmail.com>
2024-09-10 21:05:40 -0300

    aluno - listar - deletar

--- alunoonline/api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ alunoonline/api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -2,9 +2,13 @@ package br.com.alunoonline.api.controller;
 
 import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.service.AlunoService;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/alunos")
@@ -18,6 +22,22 @@ public class AlunoController {
     @ResponseStatus(HttpStatus.CREATED)
     public void criarAluno(@RequestBody Aluno aluno) {
         alunoService.criarAluno(aluno);
-
     }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> listarTodosAlunos() {
+       return alunoService.listarTodosAlunos();
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> buscarAlunoPorId(@PathVariable Long id){
+       return alunoService.buscarAlunoPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarAlunoPorId(@PathVariable Long id){
+        alunoService.deletarAlunoPorId(id);
+    }
+
 }
